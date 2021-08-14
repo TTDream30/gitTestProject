92f4cba93a877d7ace1225a2f1c171e7a173f6d0
TT <2579139437@qq.com>
2021-08-14 14:56:30 +0800

    first commit

---
+++ src/main/java/org/example/Start.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author TT
+ * @create 2021-08-14-14:54
+ */
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("yyy!");
+    }
+}
